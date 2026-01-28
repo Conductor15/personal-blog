027cc7188117ca6150d21be5a9311319e5d81085
Conductor15 <conductor15.tw@gmail.com>
2026-01-28 10:40:00 +0700

    fix: database connect

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose");
 module.exports.connect = async () => {
     try {
         // console.log("hello")
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect(process.env.MONGO_URL,{
+            minPoolSize: 1,      
+            maxPoolSize: 10,    
+            serverSelectionTimeoutMS: 5000, 
+        });
         console.log("Success");
         
     } catch (error) {
